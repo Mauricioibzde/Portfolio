637e4fda275fa501a45bc4e54d9a47cf1c360f7c
Mauricio Farias da Silva <158978545+Mauricioibzde@users.noreply.github.com>
2025-02-05 23:02:40 +0100

    Delete script directory

--- script/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let bg = document.body;
-
-    window.addEventListener("scroll", function () {
-        let scrollTop = window.scrollY;
-        bg.style.backgroundPosition = `center ${scrollTop * 0.5}px`;
-    });
-});
